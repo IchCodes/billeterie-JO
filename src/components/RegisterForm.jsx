b90f50dc3cdcd40a75421d010912d16b45c25231
IchCodes <hichame.sraidi@gmail.com>
2024-05-06 19:38:17 +0200

    remove unsed image

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -19,13 +19,6 @@ const RegisterForm = () => {
   return (
     <main class="container form-signin d-flex flex-column justify-content-center align-items-center">
         <form onSubmit={handleSubmit} > 
-          <img
-            class="mb-4"
-            src="/docs/5.3/assets/brand/bootstrap-logo.svg"
-            alt=""
-            width="72"
-            height="57"
-          />
           <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
           <div class="form-floating">
