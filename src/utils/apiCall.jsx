e91b571b3cd00d0a56d3f0d75bacd6d014363937
IchCodes <hichame.sraidi@gmail.com>
2024-05-09 20:27:03 +0200

    api functions centralized

---
+++ src/utils/apiCall.jsx
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import Cookies from 'universal-cookie';
+
+export async function getToken(username, password) {
+  const url = `${import.meta.env.VITE_BASE_AUTH_URL}/login`;
+  const body = {
+    username: username,
+    password: password
+  };
+
+  try {
+    const response = await axios.post(url, body);
+    console.log(response)
+
+    const cookies = new Cookies(null, { path: '/' });
+    cookies.set('token', response.data.token, { path: '/' });
+
+    return { success: true, message: response.data.message };
+
+  } catch (error) {
+    return { success: false, message: 'Error during the request', error: error };
+  }
+}
+
+export async function getPlans() {
+  const url = `${import.meta.env.VITE_BASE_PLANS_URL}/all`;
+
+  try {
+    const response = await axios.get(url);
+    return { success: true, data: response.data };
+  } catch (error) {
+    return { success: false, message: 'Error during the request', error: error };
+  }
+}
