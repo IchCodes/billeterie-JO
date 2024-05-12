a958275efc959a17aa8f8822d08709623539267f
IchCodes <hichame.sraidi@gmail.com>
2024-05-12 16:29:06 +0200

    page admin test

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import { jwtDecode } from "jwt-decode";
+import Cookies from "universal-cookie";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
-const admin = () => {
-  return (
-    <div>admin page protected</div>
-  )
-}
+const Admin = () => {
+    const cookies = new Cookies(null, { path: "/" });
+    const token = cookies.get("token");
+    
+    if (!token) {
+        return (
+            <>
+                <Header />
+                <div>access-denied</div>
+                <Footer />
+            </>
+        );
+    }
 
-export default admin
+    const decodedToken = jwtDecode(token);
+
+    if (decodedToken.role !== "ADMIN") {
+        return (
+            <>
+                <Header />
+                <div>access-denied</div>
+                <Footer />
+            </>
+        );
+    }
+
+    console.log(decodedToken.role);
+
+    return (
+        <>
+            <Header />
+            <div>admin page protected</div>
+            <Footer />
+        </>
+    );
+};
+
+export default Admin;
