8b47f690845141f6981b3db443d0fc3c041c5047
IchCodes <hichame.sraidi@gmail.com>
2024-05-19 03:16:46 +0200

    test jsx to PDF

--- src/pages/test/index.jsx
+++ src/pages/test/index.jsx
@@ -15,8 +15,13 @@ const Test = () => {
       const imgData = canvas.toDataURL('image/png');
       const pdf = new jsPDF('l', 'mm', 'a4');
       pdf.addImage(imgData, 'PNG', 0, 0);
-      pdf.save('ticket.pdf');
-      setloader(false);
+      html2canvas(capture).then((capture) => {
+        const imgData2 = capture.toDataURL('image/png');
+        pdf.addPage();
+        pdf.addImage(imgData2, 'PNG', 0, 0);
+        pdf.save('ticket.pdf');
+        setloader(false);
+      });
     });
       
   }
@@ -44,6 +49,41 @@ const Test = () => {
       <button onClick={downloadPDF} className="btn btn-primary">
         {loader ? 'Downloading...' : 'Download PDF'}
       </button>
+        <div className="container">
+        <div className="card">
+          <div className="card-header font-weight-bold">
+            VOTRE E-BILLET
+          </div>
+          <div className="card-body">
+          <div class="input-group has-validation">
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="username"
+                        name="username"
+                        placeholder="Username"
+                        required={true}
+                      />
+                      <span class="input-group-text">375 €</span>
+                    </div>
+                    
+            <h5 className="card-title">BORDEAUX ST JEAN / NARBONNE</h5>
+            <p className="card-text">
+              Nom : SCHNEIDER<br />
+              Prénom : MATTI<br />
+              DOSSIER VOYAGE : QVVLMI<br />
+              Référence client :<br />
+              Départ / Arrivée Date / Heure INTERCITES BILLET VALEABLE AVEC UNE CARTE LIBERTE<br />
+              BORDEAUX ST JEAN le 10/10 à 10h30 VOITURE N°4 - PLACE 83 FENETRE DUO<br />
+              NARBONNE à 13h55 CANAL PANORAMIQUE - PLACEMENT LIBRE
+            </p>
+            <img src="qrcode.png" className="float-right" alt="QR Code" />
+          </div>
+          <div className="card-footer text-muted">
+            COMMENT GERER MA RESERVATION? CONDITIONS D'UTILISATION DU E-BILLET
+          </div>
+        </div>
+      </div>
     </>
   );
 };
