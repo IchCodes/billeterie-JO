ac6012bab4bdc8d019c801076565efe2dbd5cf05
IchCodes <hichame.sraidi@gmail.com>
2024-06-05 15:15:02 +0200

    admin page

---
+++ src/components/Admin.jsx
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from "react";
+import Orders from "./Orders";
+import { getPlans } from "../utils/apiCall";
+import ModalDeletePlan from "./ModalDeletePlan";
+import ModalAddPlan from "./ModalAddPlan";
+
+const Admin = () => {
+  const [plans, setPlans] = useState([]);
+  const [showModalDeletePlan, setShowModalDeletePlan] = useState(false);
+  const [showModalAddPlan, setShowModalAddPlan] = useState(false);
+  const [currentPlan, setCurrentPlan] = useState(null);
+
+  useEffect(() => {
+    getPlans().then((response) => {
+      setPlans(response.data);
+    });
+  }, []);
+  return (
+    <>
+      <div className="container">
+        <div className="mb-3 p-3 border border-2 rounded">
+          <h1 className="text-center">Administration</h1>
+        </div>
+        <div className="accordion mb-3">
+          <div className="accordion-item">
+            <h2 className="accordion-header">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseOrders"
+                aria-expanded="false"
+                aria-controls="collapseOrders"
+              >
+                Commandes
+              </button>
+            </h2>
+            <div
+              id="collapseOrders"
+              className="accordion-collapse collapse"
+              data-bs-parent="#accordionExample"
+            >
+              <div className="accordion-body">
+                <Orders />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="accordion">
+          <div className="accordion-item">
+            <h2 className="accordion-header">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapsePlans"
+                aria-expanded="false"
+                aria-controls="collapseOrders"
+              >
+                Gestion des Offres
+              </button>
+            </h2>
+            <div
+              id="collapsePlans"
+              className="accordion-collapse collapse"
+              data-bs-parent="#accordionExample"
+            >
+              <div className="accordion-body">
+                <div className="row">
+                  {plans.map((plan) => (
+                    <div key={plan.id} className="col-md-4">
+                      <div className="card mb-4">
+                        <img
+                          src={plan.image_url}
+                          className="card-img-top"
+                          alt={plan.plan}
+                        />
+                        <div className="card-body">
+                          <h5 className="card-title">{plan.plan}</h5>
+                          <p className="card-text">{`Donne accès à ${plan.ticket_quantity} personne.`}</p>
+                          <p className="card-text">Prix: {plan.price} €</p>
+                          <button className="btn btn-primary me-2 mb-2">
+                            Modifier offre
+                          </button>
+                          <button
+                            className="btn btn-danger"
+                            onClick={() => {
+                              setShowModalDeletePlan(true);
+                              setCurrentPlan(plan);
+                            }}
+                          >
+                            Supprimer offre
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                  <button
+                    className="btn btn-success"
+                    onClick={() => {
+                      setShowModalAddPlan(true);
+                    }}
+                  >
+                    Créer offre
+                  </button>
+                  <ModalDeletePlan
+                    showModal={showModalDeletePlan}
+                    setShowModal={setShowModalDeletePlan}
+                    modalTitle="Supprimer Offre"
+                    plan={currentPlan}
+                    setPlans={setPlans}
+                  />
+
+                  <ModalAddPlan
+                    showModal={showModalAddPlan}
+                    setShowModal={setShowModalAddPlan}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Admin;
